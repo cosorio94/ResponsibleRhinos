c463a81682e80311fcce063466f46987f07107eb
Carlos Osorio <carlososoriov@gmail.com>
2017-09-04 22:32:10 -0700

    modularized MapView

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,161 +1,12 @@
 import React, { Component } from 'react';
-import {GoogleApiWrapper} from 'google-maps-react';
 import { render } from 'react-dom';
-import axios from 'axios';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Header from './components/header.jsx';
-import MapContainer from './components/mapContainer.jsx';
+import MapView from './components/mapView.jsx';
 import UserPage from './components/userpage.jsx';
-import PinInfo from './components/pininfo.jsx';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 injectTapEventPlugin();
-
-class mapView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentCenter: {
-        lat: 37.774929,
-        lng: -122.41941600000001
-      },
-      zoom: 15,
-      currentUser: null,
-      markers: [],
-      mapId: null,
-      currPin: null
-    };
-    this.updateCenter = this.updateCenter.bind(this);
-    this.updateZoom = this.updateZoom.bind(this);
-    this.addMarker = this.addMarker.bind(this);
-    this.save = this.save.bind(this);
-    this.github = this.github.bind(this);
-    this.replaceURL = (id) => props.history.push(`?=${id}`);
-    this.setCurrPin = this.setCurrPin.bind(this);
-    this.updateCurrPinInfo = this.updateCurrPinInfo.bind(this);
-  }
-
-  setCurrPin(index) {
-    this.setState({
-      currPin: index
-    });
-  }
-
-  componentDidMount() {
-    let mapId = window.location.href.split('=')[1];
-    if (mapId) {
-      this.fetch(mapId);
-    }
-    axios.get('/user/signedIn')
-      .then((res) => {
-        this.setState({
-          currentUser: res.data[0].user_name
-        });
-      })
-      .catch(err => console.log('signedIn error:', err));
-  }
-
-  // Changed this function to accept a marker object instead of only a position.
-  addMarker(marker){
-    var markers = this.state.markers;
-    markers.push(marker);
-    this.setState({
-      markers: markers
-    });
-  }
-
-
-  github() {
-    axios.get('/auth/github')
-      .then(res => {
-        console.log('github response:', res);
-      })
-      .catch(err => console.log('ERROR:', err));
-  }
-
-  update() {
-    let state = JSON.stringify(this.state);
-    let mapId = window.location.href.split('=')[1];
-    axios.put(`/map/${id}`, {state: state})
-      .then((res) => {
-        console.log(res);
-      })
-      .catch(err => console.log('put error:', err));
-  }
-
-  save() {
-    let state = JSON.stringify(this.state);
-    axios.post('/map', {state: state})
-      .then(res => {
-        this.setState({
-          mapId: res.data
-        });
-        console.log("Data is:", res.data);
-        this.replaceURL(res.data);
-      })
-      .catch(err => console.log(err));
-  }
-
-  fetch(id) {
-    axios.get(`/map/${id}`)
-      .then(res => {
-        this.setState(res.data);
-      })
-      .catch(err => console.log('get error:', err));
-
-  }
-
-  updateCenter(center) {
-    this.setState({
-      currentCenter: center
-    });
-  }
-
-  updateZoom(zoom) {
-    this.setState( {
-      zoom: zoom
-    });
-  }
-
-  updateCurrPinInfo(text){
-    let markers = this.state.markers;
-    markers[this.state.currPin].info = text;
-    this.setState({
-      markers: markers
-    });
-  }
-
-  render() {
-    return (
-      <MuiThemeProvider>
-        <div>
-          <Header
-            save={this.save}
-            git={this.github}
-            currentUser={this.state.currentUser}
-          />
-          <div style={{height: '0.5em'}}>
-          </div>
-          <MapContainer
-            currentCenter={this.state.currentCenter}
-            updateCenter={this.updateCenter}
-            updateZoom={this.updateZoom}
-            markers={this.state.markers}
-            addMarker={this.addMarker}
-            zoom={this.state.zoom}
-            setCurrPin={this.setCurrPin}
-          />
-          {this.state.currPin !== null &&
-            <PinInfo text={this.state.markers[this.state.currPin].info}
-                     updateCurrPinInfo={this.updateCurrPinInfo}/>
-          }
-          
-        </div>
-      </MuiThemeProvider>
-    );
-  }
-}
 
 const userView = ({match}) => (
   <MuiThemeProvider>
@@ -172,7 +23,7 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Route exact path='/' component={mapView} />
+          <Route exact path='/' component={MapView} />
           <Route path='/user' component={userView} />
         </div>
       </Router>
